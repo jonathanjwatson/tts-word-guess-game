68900869acfc9685b8c439ba9edefe5d4dc6c779
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-06-06 19:35:13 -0400

    dynamically generating the letter bank

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 console.log("Hello world!");
 
 // JavaScript Variables
-
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 const wordToGuess = ["b", "a", "n", "a", "n", "a"];
 
 // DOM Variables
 const wordToGuessDiv = document.getElementById("word-to-guess");
+const letterBankDiv = document.getElementById("letter-bank");
 
 // Function Definitions
 function addWordToGuess() {
@@ -21,6 +49,21 @@ function addWordToGuess() {
     wordToGuessDiv.append(newDiv);
   }
 }
+
+function generateLetterBank() {
+  for (let i = 0; i < letters.length; i++) {
+    const newDiv = document.createElement("div");
+    const newButton = document.createElement("button");
+
+    newDiv.className = "col-sm-2 col-xs-1";
+    newButton.innerText = letters[i];
+    newButton.className = "btn btn-primary";
+
+    newDiv.append(newButton);
+    letterBankDiv.append(newDiv);
+  }
+}
 // Function Calls
 addWordToGuess();
+generateLetterBank();
 // Event Listeners
