c4fda52fa4e522cba5346494b457fc2542c898aa
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-06-06 19:21:14 -0400

    dynamically generating underscore for word to guess

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hello world!");
+
+// JavaScript Variables
+
+const wordToGuess = ["b", "a", "n", "a", "n", "a"];
+
+// DOM Variables
+const wordToGuessDiv = document.getElementById("word-to-guess");
+
+// Function Definitions
+function addWordToGuess() {
+  for (let i = 0; i < wordToGuess.length; i++) {
+    // 1. Create a new element.
+    const newDiv = document.createElement("div");
+    const newH1 = document.createElement("h1");
+    // 2. Add content
+    newDiv.className = "col";
+    newH1.innerText = "_";
+    // 3. Append to an existing element
+    newDiv.append(newH1);
+    wordToGuessDiv.append(newDiv);
+  }
+}
+// Function Calls
+addWordToGuess();
+// Event Listeners
